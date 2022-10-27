c12e7287e680f257440cde1776ca5354213d0be2
Aigaplee@gmail.com <Aigaplee@gmail.com>
2022-10-27 16:49:50 +0600

    Статистика успеваемости

--- student_management_app/models.py
+++ student_management_app/models.py
@@ -1,3 +1,5 @@
+from tkinter import CASCADE
+from unittest.util import _MAX_LENGTH
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.signals import post_save
@@ -45,17 +47,14 @@ class Courses(models.Model):
     # def __str__(self):
 	#     return self.course_name
 
-
-
-class Subjects(models.Model):
-    id =models.AutoField(primary_key=True)
+class Subjects(models.Models):
+    id = models.AutoField(primary_key=True)
     subject_name = models.CharField(max_length=255)
-    course_id = models.ForeignKey(Courses, on_delete=models.CASCADE, default=1) #need to give defauult course
+    course_id = models.ForeignKey(Courses, on_delete=models.CASCADE, default=1)
     staff_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True)
     objects = models.Manager()
-
 
 
 class Students(models.Model):
